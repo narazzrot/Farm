230ee651345f0af4d1d15cdb7381543ed05835c1
narazzrot <48467467+narazzrot@users.noreply.github.com>
2020-01-05 15:48:14 +0900

    Add files via upload

---
+++ ICTJAVASCRIPT.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+
+const canvas = 
+document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'dark_blue';
+ctx.fillRect(10, 10, 150, 100);
+
